ddaf4a7fd7782d8ab6b3ddb25b39a9fff82622b7
shotaro0726 <shoutaro0726@gmail.com>
2020-03-20 10:00:15 +0900

    First Commit

---
+++ js/shotaro.js
@@ -0,0 +1,32 @@
+// Dark Mode Switcher
+// Source: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+const toggleSwitch = document.querySelector('.dark_mode_switch input[type="checkbox"]');
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Store the user preference for future visits
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+// Check for saved user preference, if any, load on website reopening
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
